2a0a77e710a8f53382f9b70309c43af8fe082b37
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-17 14:11:03 +0200

    fix: improve a11y, images and scripts

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,36 +1,38 @@
 import "fslightbox";
 import Macy from "macy";
 
-const bricksContainer = document.querySelector("#bricks-container");
-const bricksContainerReleaseBtn = document.querySelector(
-  "#bricks-container-release",
-);
-const bricksContainerOverlay = document.querySelector(
-  "#bricks-container-overlay",
-);
+(() => {
+  const bricksContainer = document.querySelector("#bricks-container");
+  const bricksContainerReleaseBtn = document.querySelector(
+    "#bricks-container-release",
+  );
+  const bricksContainerOverlay = document.querySelector(
+    "#bricks-container-overlay",
+  );
 
-bricksContainerReleaseBtn.addEventListener("click", () => {
-  bricksContainer.classList.remove("max-h-[1000px]");
-  bricksContainer.classList.add("max-h-[99999px]");
-  bricksContainerOverlay.classList.remove("opacity-100");
-  bricksContainerOverlay.classList.add("opacity-0");
+  bricksContainerReleaseBtn.addEventListener("click", () => {
+    bricksContainer.classList.remove("max-h-[1000px]");
+    bricksContainer.classList.add("max-h-[99999px]");
+    bricksContainerOverlay.classList.remove("opacity-100");
+    bricksContainerOverlay.classList.add("opacity-0");
 
-  setTimeout(() => {
-    bricksContainerOverlay.remove();
-    bricksContainer.classList.remove("max-h-[99999px]");
-  }, 2000);
-});
+    setTimeout(() => {
+      bricksContainerOverlay.remove();
+      bricksContainer.classList.remove("max-h-[99999px]");
+    }, 2000);
+  });
 
-const macyInstance = Macy({
-  container: ".macy",
-  mobileFirst: true,
-  columns: 1,
-  breakAt: {
-    768: 2,
-    1040: 3,
-  },
-  margin: {
-    x: 42,
-    y: 42,
-  },
-});
+  const macyInstance = Macy({
+    container: ".macy",
+    mobileFirst: true,
+    columns: 1,
+    breakAt: {
+      768: 2,
+      1040: 3,
+    },
+    margin: {
+      x: 42,
+      y: 42,
+    },
+  });
+})();
