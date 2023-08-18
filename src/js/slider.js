7fe86180148a2a80099a87ef4fa4c0eb51e1bf8d
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-18 12:21:03 +0200

    refactor: make slider reusable

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,48 +1,51 @@
 (() => {
-  const slider = document.querySelector("#slider");
-  const sliderTrack = slider.querySelector("[data-slider-track]");
-  const prevBtn = slider.querySelector("[data-slider-prev]");
-  const nextBtn = slider.querySelector("[data-slider-next]");
-  const slides = slider.querySelectorAll("[data-slide]");
-  const sliderNavigation = slider.querySelector("[data-slider-navigation");
+  const sliders = document.querySelectorAll("[data-slider]");
 
-  if (slides.length === 1) {
-    sliderNavigation.remove();
-  }
+  sliders.forEach((slider) => {
+    const sliderTrack = slider.querySelector("[data-slider-track]");
+    const prevBtn = slider.querySelector("[data-slider-prev]");
+    const nextBtn = slider.querySelector("[data-slider-next]");
+    const slides = slider.querySelectorAll("[data-slide]");
+    const sliderNavigation = slider.querySelector("[data-slider-navigation");
 
-  sliderTrack.scrollTo(0, 0);
-  let currentSlide = 0;
-
-  const setAriaSelected = (slideNumber) => {
-    slides.forEach((slide) => slide.setAttribute("aria-selected", "false"));
-    slides[slideNumber].setAttribute("aria-selected", "true");
-  };
-
-  nextBtn.addEventListener("click", () => {
-    if (currentSlide === slides.length - 1) {
-      sliderTrack.scrollTo(0, 0);
-      currentSlide = 0;
-      setAriaSelected(currentSlide);
-      return;
+    if (slides.length === 1) {
+      sliderNavigation.remove();
     }
 
-    if (currentSlide < slides.length - 1) {
-      sliderTrack.scrollTo(slides[currentSlide + 1].offsetLeft, 0);
-      currentSlide += 1;
-      setAriaSelected(currentSlide);
-    }
-  });
+    sliderTrack.scrollTo(0, 0);
+    let currentSlide = 0;
 
-  prevBtn.addEventListener("click", () => {
-    if (currentSlide === 0) {
-      sliderTrack.scrollTo(slides[slides.length - 1].offsetLeft, 0);
-      currentSlide = slides.length - 1;
-      setAriaSelected(currentSlide);
-      return;
-    }
+    const setAriaSelected = (slideNumber) => {
+      slides.forEach((slide) => slide.setAttribute("aria-selected", "false"));
+      slides[slideNumber].setAttribute("aria-selected", "true");
+    };
 
-    sliderTrack.scrollTo(slides[currentSlide - 1].offsetLeft, 0);
-    currentSlide -= 1;
-    setAriaSelected(currentSlide);
+    nextBtn.addEventListener("click", () => {
+      if (currentSlide === slides.length - 1) {
+        sliderTrack.scrollTo(0, 0);
+        currentSlide = 0;
+        setAriaSelected(currentSlide);
+        return;
+      }
+
+      if (currentSlide < slides.length - 1) {
+        sliderTrack.scrollTo(slides[currentSlide + 1].offsetLeft, 0);
+        currentSlide += 1;
+        setAriaSelected(currentSlide);
+      }
+    });
+
+    prevBtn.addEventListener("click", () => {
+      if (currentSlide === 0) {
+        sliderTrack.scrollTo(slides[slides.length - 1].offsetLeft, 0);
+        currentSlide = slides.length - 1;
+        setAriaSelected(currentSlide);
+        return;
+      }
+
+      sliderTrack.scrollTo(slides[currentSlide - 1].offsetLeft, 0);
+      currentSlide -= 1;
+      setAriaSelected(currentSlide);
+    });
   });
 })();
