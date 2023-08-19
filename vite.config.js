c3ba880f849d6dda322b5490990e44fd77ba8000
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-19 12:08:49 +0200

    fix: add font-display: swap param to font

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import webfontDownload from "vite-plugin-webfont-dl";
 export default {
   plugins: [
     webfontDownload([
-      "https://fonts.googleapis.com/css2?family=Inter:wght@400;500",
+      "https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap",
       "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap",
     ]),
   ],
