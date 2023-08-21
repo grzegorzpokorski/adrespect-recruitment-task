66a525a65cd1b4b9ff57491ded8c61fcdb8cfc10
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-08-21 22:12:49 +0200

    feat: add animations

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,10 @@ import "../css/style.css";
 import "./gallery.js";
 import "./slider.js";
 import { initNavigation } from "./navigation.js";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 initNavigation();
+AOS.init({
+  disable: window.matchMedia("(prefers-reduced-motion: reduce)").matches,
+});
